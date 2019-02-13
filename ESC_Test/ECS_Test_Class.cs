6331b4cb8eeccf5e385c478aa860e41a5e7cb3e3
TroelsBleicken <41672824+TroelsBleicken@users.noreply.github.com>
2019-02-13 11:28:49 +0100

    Så er der endeligt tests

--- ESC_Test/ECS_Test_Class.cs
+++ ESC_Test/ECS_Test_Class.cs
@@ -3,20 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ECS.Refactored.Random;
+using ECS.Refactored.TempSensor;
 using NUnit;
 using NUnit.Framework;
-using ECS.Refactored;
+
 
 namespace ECS_Test
 {
+    using ECS.Refactored.ECS;
     [TestFixture]
     class ECS_Test_Class
     {
         private ECS uut;
-        [SetUp]
-        void Setup()
-        {
+        private TempSensor _temp;
+        private IRandom rand;
 
+        [TestCase(10, 10)]
+        [TestCase(5, 10)]
+        public void TestTempAlwaysReturn10(int num, int result)
+        {
+            rand = new FakeRandom();
+            Assert.That(rand.NextRandomNumber(num, num), Is.EqualTo(result));
+        }
+
+        [TestCase(10)]
+        public void TestTemp(int result)
+        {
+            rand = new FakeRandom();
+            _temp = new TempSensor(rand);
+            Assert.That(_temp.GetTemp(), Is.EqualTo(result));
         }
     }
 }
